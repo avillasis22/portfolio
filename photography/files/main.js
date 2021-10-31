4738218a2da454d014660c8ba517940c43667bc3
avillasis22 <aidan.villasis@gmail.com>
2021-10-31 15:11:48 -0400

    test

---
+++ photography/files/main.js
@@ -0,0 +1,9 @@
+sr.reveal('.image, .caption, .bioDiv', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.1,
+});
