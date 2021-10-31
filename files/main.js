c7f7674649d13ea77287004c4254cf02b6aff162
avillasis22 <aidan.villasis@gmail.com>
2021-10-31 14:58:14 -0400

    Portfolio

---
+++ files/main.js
@@ -0,0 +1,10 @@
+
+sr.reveal('.bioHeader, .portfolio, .portfolioCaption, .bioText', {
+    origin: 'bottom',
+    distance: '30px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.2,
+});
