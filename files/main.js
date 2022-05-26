63294296085ce6e33384b790897710f186b819bc
avillasis22 <aidan.villasis@gmail.com>
2022-05-26 13:39:44 -0400

    Homepage Improvements
    - Added improved full-screen navigation menu
    -improved mobile scaling
    -Added email tag as footer

--- files/main.js
+++ files/main.js
@@ -1,3 +1,30 @@
+$(document).ready(function() {
+
+    $('body').css('display', 'none');
+
+    $('body').fadeIn(1000);
+
+
+
+    $('.link').click(function() {
+
+        event.preventDefault();
+
+        newLocation = this.href;
+
+        $('body').fadeOut(1000, newpage);
+
+    });
+
+    function newpage() {
+
+        window.location = newLocation;
+
+    }
+
+});
+
+
 sr.reveal('.bioHeader, .portfolio, .portfolioCaption, .bioText', {
     origin: 'bottom',
     distance: '30px',
@@ -38,32 +65,3 @@ function scrollFunction() {
         document.getElementById("navbar").style.top = "-90px";
     }
 }
-
-/*window.onmousemove = function(e) {
-
-    if (e.target.classList.contains('dot')) {
-    		//	Content of dot which is hovered
-        var $target = e.target.nextElementSibling;
-
-        //	Only work with hovered target
-        //	Add visible class hovered elem to distinguish
-        if (!$target.classList.contains('visible')) {
-        		$target.classList.add('visible');
-        } else {
-            //	Get viewport offset of tooltip element
-            var offset = $target.parentElement.getBoundingClientRect();
-            //	Tooltip distance from mouse(px)
-            var tipDist = 15;
-            //	Calc and set new tooltip location
-            $target.style.top = (e.clientY - offset.top + tipDist) + 'px';
-            $target.style.left = (e.clientX - offset.left + tipDist) + 'px';
-        }
-    } else {
-    		//	Remove visible class
-        var content = document.getElementsByClassName('content');
-        for (var i = 0; i < content.length; i++) {
-        		content[i].classList.remove('visible');
-        }
-    }
-
-};*/
