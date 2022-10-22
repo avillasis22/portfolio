ba6aab438df13e5d3386e5c105d51bfa38c2830a
avillasis22 <aidan.villasis@gmail.com>
2022-10-22 13:47:28 -0400

    Added navigation menu to all pages
    - added navigation menu to all pages
    
    - began adding fadein effect to all pages

--- lungcancer/files/main.js
+++ lungcancer/files/main.js
@@ -1470,3 +1470,41 @@
         }), 1e3)
     }()
 }();
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+    $('body').css('display', 'none');
+    $('body').fadeIn(500);
+
+});
+
+$('a').click(function(e) {
+    e.preventDefault();
+    newLocation = this.href;
+    $('html').fadeOut('slow', newpage);
+});
+
+function newpage() {
+    window.location = newLocation;
+};
+
+window.addEventListener('load', function() {
+    document.querySelector('body').classList.add("loaded");
+    document.getElementById("loader-wrapper").style.zIndex = "0";
+});
+
+if(performance.navigation.type == 2){
+   location.reload(true);
+}
--- photography/files/main.js
+++ photography/files/main.js
@@ -1,3 +1,34 @@
+$(document).ready(function() {
+
+    $('body').css('display', 'none');
+    $('body').fadeIn(500);
+
+});
+
+$('a').click(function(e) {
+    e.preventDefault();
+    newLocation = this.href;
+    $('html').fadeOut('slow', newpage);
+});
+
+function newpage() {
+    window.location = newLocation;
+};
+
+window.addEventListener('load', function() {
+    document.querySelector('body').classList.add("loaded");
+    document.getElementById("loader-wrapper").style.zIndex = "0";
+});
+
+if(performance.navigation.type == 2){
+   location.reload(true);
+}
+
+
+
+
+
+
 sr.reveal('.image, .caption, .bioDiv', {
     origin: 'bottom',
     distance: '50px',
