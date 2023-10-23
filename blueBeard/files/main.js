c5803cd44cba4235ea5fa61ca5d6bdc4054e1ba7
avillasis22 <aidan.villasis@gmail.com>
2023-10-22 21:59:03 -0400

    Bluebeard (FR)
    - Added Bluebeard page for French
    - Updated "freshman" to "sophomore"

---
+++ blueBeard/files/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $('body').css('display', 'none');
+    $('body').fadeIn(500);
+
+});
+
+$('a').click(function(e) {
+    e.preventDefault();
+    newLocation = this.href;
+    $('html').fadeOut('slow', newpage);
+});
+
+function newpage() {
+    window.location = newLocation;
+};
+
+window.addEventListener('load', function() {
+    document.querySelector('body').classList.add("loaded");
+    document.getElementById("loader-wrapper").style.zIndex = "0";
+});
+
+if(performance.navigation.type == 2){
+   location.reload(true);
+}
+
+
+
+
+
+
+sr.reveal('.image, .caption, .bioDiv, .captionTemp, .subCaption2, .end, .endText2', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 800,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.1,
+});
